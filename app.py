63e1af127deb509b8a4c676622bd1a79dfcf94cd
Athira1903 <athirabiju2027@mca.ajce.in>
2025-04-08 10:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "🚀 Hello from Render + Docker + Flask!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return render_template("index.html")
