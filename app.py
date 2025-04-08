96cfe6f5ac79e291620bbe2ff506eb96def62093
Athira1903 <athirabiju2027@mca.ajce.in>
2025-04-08 11:25:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    return send_file('index.html')
